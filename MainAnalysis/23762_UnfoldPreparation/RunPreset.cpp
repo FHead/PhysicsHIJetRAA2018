fe95414b5822dbeceb165ee3c9020b282ce8a2b7
FHead <chen.yi.first@gmail.com>
2021-10-05 16:44:12 +0200

    change binning to global setting

--- MainAnalysis/23762_UnfoldPreparation/RunPreset.cpp
+++ MainAnalysis/23762_UnfoldPreparation/RunPreset.cpp
@@ -26,6 +26,9 @@ int main(int argc, char *argv[])
    vector<string> JetR       = CL.GetStringVector("JetR", {"1", "2", "3", "4", "5", "6", "7", "8", "9"});
    vector<string> Centrality = CL.GetStringVector("Centrality", {"0to10", "10to30", "30to50", "50to90"});
 
+   Binnings["GenBins"]       = CL.Get("GenBins", Binnings["GenBins"]);
+   Binnings["RecoBins"]      = CL.Get("RecoBins", Binnings["RecoBins"]);
+
    for(string R : JetR)
    {
       for(string C : Centrality)
@@ -37,8 +40,8 @@ int main(int argc, char *argv[])
          cout << "time ./Execute --MC " << MCFile << " --Data " << DataFile
             << " --Output Output/" << Prefix << "_" << RC << "_" << Suffix << ".root"
             << " --Observable JetPT --ObservableIndex -1 --Binning None --BinningIndex -1"
-            << " --ObservableGenBins " << Binnings["GenJetPTBins"]
-            << " --ObservableRecoBins " << Binnings["RecoJetPTBins"]
+            << " --ObservableGenBins " << Binnings["GenBins"]
+            << " --ObservableRecoBins " << Binnings["RecoBins"]
             << " --BinningGenBins 0,1 --BinningRecoBins 0,1"
             << " --ObservableUncertaintyShift " << JetShift
             << " --ObservableUncertaintySmear " << JetSmear
@@ -54,8 +57,8 @@ map<string, string> GetBinnings()
 {
    map<string, string> Binnings;
 
-   Binnings["GenJetPTBins"] = "20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,110,120,130,140,150,160,170,180,190,200,220,240,260,280,300,325,350,375,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850,875,900,925,950,975,1000,1100,1200,1300,1400,1500";
-   Binnings["RecoJetPTBins"] = "20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,110,120,130,140,150,160,170,180,190,200,220,240,260,280,300,325,350,375,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850,875,900,925,950,975,1000,1100,1200,1300,1400,1500";
+   Binnings["GenBins"] = "20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,110,120,130,140,150,160,170,180,190,200,220,240,260,280,300,325,350,375,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850,875,900,925,950,975,1000,1100,1200,1300,1400,1500";
+   Binnings["RecoBins"] = "20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,110,120,130,140,150,160,170,180,190,200,220,240,260,280,300,325,350,375,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850,875,900,925,950,975,1000,1100,1200,1300,1400,1500";
 
    return Binnings;
 }
