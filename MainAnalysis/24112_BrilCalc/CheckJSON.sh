75add5c0178452e382b9022b9bfe530d1e1f9c27
FHead <chen.yi.first@gmail.com>
2021-10-12 16:54:14 +0200

    code to extract luminosity and put into the dh file

---
+++ MainAnalysis/24112_BrilCalc/CheckJSON.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+Input=$1
+
+brilcalc lumi --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_BRIL.json -i ${Input} -o temp.csv
+BrilValue=`grep -v "^#" temp.csv | cut -d ',' -f 6 | Sum`
+
+brilcalc lumi --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -i ${Input} -o temp.csv
+PhysicsValue=`grep -v "^#" temp.csv | cut -d ',' -f 6 | Sum`
+
+rm temp.csv
+
+Tag=`basename $Input | sed "s/_Nominal//" | sed "s/_JSON.txt//"`
+
+echo $Tag $BrilValue $PhysicsValue
+
+DHSet GlobalSetting.dh Lumi ${Tag}_BRIL double $BrilValue
+DHSet GlobalSetting.dh Lumi ${Tag}_PHYSICS double $PhysicsValue
+
