bfa9dcabed26f8ebb40a33863fdcd56e01f223c7
FHead <chen.yi.first@gmail.com>
2021-10-13 16:19:41 +0200

    unfold iteration

---
+++ MainAnalysis/23772_UnfoldIteration/RunFile.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+Prefix=$1
+Suffix=$2
+IsPP=$3
+
+JetR=`DHQuery GlobalSetting.dh Global JetR | sed 's/"//g'`
+Centrality=`DHQuery GlobalSetting.dh Global Centrality | sed 's/"//g'`
+if [[ "$IsPP" == "1" ]]; then
+   Centrality="Inclusive"
+fi
+
+Ignore=`DHQuery GlobalSetting.dh Binning PTUnderflow`
+
+for R in $JetR
+do
+   for C in $Centrality
+   do
+	   ./Execute --Input Input/${Prefix}_R${R}_Centrality${C}_${Suffix}.root \
+         --Output Output/${Prefix}_R${R}_Centrality${C}_${Suffix}.pdf \
+         --Ignore ${Ignore} --Reference HMCTruth \
+         --State ${Prefix} --Key BestIteration_R${R}_Centrality${C}
+   done
+done
+
+
