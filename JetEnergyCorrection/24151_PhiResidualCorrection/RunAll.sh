a80f794393444a8881fb2832894e9a463dfd27e7
FHead <chen.yi.first@gmail.com>
2021-11-10 12:29:01 +0100

    phi residual correction code

---
+++ JetEnergyCorrection/24151_PhiResidualCorrection/RunAll.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+JetR=`DHQuery GlobalSetting.dh Global JetR | sed 's/"//g'`
+
+for R in $JetR
+do
+   RValue=`DHQuery GlobalSetting.dh JetR $R`
+   # ./Execute --Input `ls Input/TestRunMCJEC_R${R}_* | head -n10 | tr '\n' ','` --Output Meow_R${R}.pdf \
+   #    --Eta -2.0,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.0 --Exclusion -0.5,1.5,0,1,-2,-1,-1.8,-0.9 \
+   #    --NPT 9 --NRho 8 --NPhi 100 --JetR ${RValue} \
+   #    --Correction PhiCorrection_AK${R}PF.txt --BinnedCorrection true \
+   #    --BaseOnGen false
+   ./Execute --Input `ls Input/TestRunMCJEC_R${R}_* | head -n10 | tr '\n' ','` --Output MeowGen_R${R}.pdf \
+      --Eta -2.0,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.0 --Exclusion -0.5,1.5,0,1,-2,-1,-1.8,-0.9 \
+      --NPT 9 --NRho 8 --NPhi 100 --JetR ${RValue} \
+      --Correction PhiCorrectionGen_AK${R}PF.txt --BinnedCorrection true \
+      --BaseOnGen true
+done
+
