1d15d480f79dbd0075d87213d8910a5b62482d9f
FHead <chen.yi.first@gmail.com>
2021-12-02 21:43:47 +0100

    ASDF

--- JetEnergyResolution/24133_StoredJetResolution/Run.sh
+++ JetEnergyResolution/24133_StoredJetResolution/Run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# for i in 1 2 3 4 5 6 7 8 9
-# do
-#    ./Execute --Input Input/TestRunPPMC_R${i}_CentralityInclusive.root --Fraction 0.001 \
-#       --Output ResultPPR${i}.pdf
-# done
+for i in 1 2 3 4 5 6 7 8 9
+do
+   ./Execute --Input Input/PPMC_R${i}_CentralityInclusive.root --Fraction 0.01 \
+      --Output ResultPPR${i}.pdf
+done
 
 # for i in 1 2 3 4 5 6 7 8 9
 # do
@@ -52,13 +52,13 @@
 #    done
 # done
 
-for i in 1 2 3 4 5 6 7 8 9
-do
-   for j in 0to10 10to30 30to50 50to90
-   do
-      ./Execute --Input Input/PbPbMCJEC_R${i}_Centrality${j}.root --Fraction 1 \
-         --Output ResultAANoPhiNoExcludeR${i}C${j}.pdf
-   done
-done
+# for i in 1 2 3 4 5 6 7 8 9
+# do
+#    for j in 0to10 10to30 30to50 50to90
+#    do
+#       ./Execute --Input Input/PbPbMCJEC_R${i}_Centrality${j}.root --Fraction 1 \
+#          --Output ResultAANoPhiNoExcludeR${i}C${j}.pdf
+#    done
+# done
 
 
