971c0cb2a2f3f68de75d3176aa748425cca5e6e4
FHead <chen.yi.first@gmail.com>
2021-12-07 13:58:29 +0100

    MB count bug fix

--- MainAnalysis/24121_MBCounting/ExportRunLumi.cpp
+++ MainAnalysis/24121_MBCounting/ExportRunLumi.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <map>
 using namespace std;
 
 #include "TFile.h"
@@ -6,7 +8,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-   TFile File("MergedMB.root");
+   TFile File("MergedMB0.root");
 
    TTree *Tree = (TTree *)File.Get("MBCounter/MBTree");
 
@@ -14,12 +16,43 @@ int main(int argc, char *argv[])
    Tree->SetBranchAddress("Run", &Run);
    Tree->SetBranchAddress("Lumi", &Lumi);
 
+   map<int, vector<int>> Lumis;
+
    int EntryCount = Tree->GetEntries();
    for(int iE = 0; iE < EntryCount; iE++)
    {
       Tree->GetEntry(iE);
-      cout << Run << " " << Lumi << endl;
+      // cout << "RunLumi " << Run << " " << Lumi << endl;
+
+      Lumis[Run].push_back(Lumi);
    }
+   
+   cout << "{";
+   for(auto R : Lumis)
+   {
+      sort(R.second.begin(), R.second.end());
+   
+      int Before = -1;
+
+      cout << "\"" << R.first << "\":[";
+      for(int i = 0; i < R.second.size(); i++)
+      {
+         int Now = R.second[i];
+
+         if(Before < 0)   // first one
+            cout << "[" << Now << ",";
+         else
+         {
+            if(Now != Before + 1)
+               cout << Before << "],[" << Now << ",";
+         }
+
+         Before = Now;
+
+      }
+      cout << Before << "]],";
+   }
+   cout << "}" << endl;
 
    File.Close();
 
