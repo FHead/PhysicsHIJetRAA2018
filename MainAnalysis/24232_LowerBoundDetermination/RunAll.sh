f070454104d0d61e0f6a6b2e045efdc948dab8ae
FHead <chen.yi.first@gmail.com>
2021-12-14 19:15:30 +0100

    first version of trigger-based lower bound setting

---
+++ MainAnalysis/24232_LowerBoundDetermination/RunAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for R in 1 2 3 4 5 6 7 8 9
+do
+   for C in 0to10 10to30 30to50 50to90 Inclusive
+   do
+      ./Execute --DHFile GlobalSetting.dh --Base R${R}_Centrality${C} \
+         --DoTrigger true --TriggerPercentage 0.99
+   done
+done
