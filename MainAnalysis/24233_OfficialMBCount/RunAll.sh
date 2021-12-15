04d4630c6cf17b05dcdf261694c4c6ce4bb62f7c
FHead <chen.yi.first@gmail.com>
2021-12-15 11:08:10 +0100

    official MB count

--- MainAnalysis/24233_OfficialMBCount/RunAll.sh
+++ MainAnalysis/24233_OfficialMBCount/RunAll.sh
@@ -10,16 +10,15 @@ for R in $JetR
 do
    for C in $Centrality
    do
-      echo $R $C
-      # PbPbData_R9_Centrality30to50
+      # echo $R $C
 
       Lumi=`DHQuery GlobalSetting.dh Lumi PbPbData_R${R}_Centrality${C}_BRIL | tr -d '"'`
       CMin=`DHQuery GlobalSetting.dh CentralityMin $C`
       CMax=`DHQuery GlobalSetting.dh CentralityMax $C`
       CRange=`echo $CMax | SubtractConst $CMin`
 
-      echo "Lumi: $Lumi $OfficialLumi"
-      echo "CRange: $CMin $CMax $CRange"
+      # echo "Lumi: $Lumi $OfficialLumi"
+      # echo "CRange: $CMin $CMax $CRange"
 
       NMB=`echo $OfficialCount | DivideConst $OfficialLumi | MultiplyConst $Lumi`
       NMB=`echo $NMB | DivideConst 0.9 | MultiplyConst $CRange`
