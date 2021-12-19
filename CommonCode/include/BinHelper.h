aab648f3717dc38e3f302325c56e3c6f5998eef7
FHead <chen.yi.first@gmail.com>
2021-12-19 15:19:26 +0100

    bin helper update

--- CommonCode/include/BinHelper.h
+++ CommonCode/include/BinHelper.h
@@ -4,10 +4,15 @@
 #include <iostream>
 #include <algorithm>
 
+#include "TFile.h"
+#include "TKey.h"
 #include "TH1D.h"
+#include "TH2D.h"
 
 std::vector<double> DetectBins(TH1D *HMin, TH1D *HMax);
 std::vector<double> ParseList(std::string List);
+TH1D *ForwardFold(TH1D *HGen, TH2D *HResponse);
+std::vector<int> ListIterations(std::string FileName);
 
 std::vector<double> DetectBins(TH1D *HMin, TH1D *HMax)
 {
@@ -60,3 +65,71 @@ std::vector<double> ParseList(std::string List)
 
    return Result;
 }
+
+TH1D *ForwardFold(TH1D *HGen, TH2D *HResponse)
+{
+   if(HGen == nullptr || HResponse == nullptr)
+      return nullptr;
+
+   static int Count = 0;
+   Count = Count + 1;
+
+   int NGen = HResponse->GetNbinsY();
+   int NReco = HResponse->GetNbinsX();
+
+   TH1D *HResult = new TH1D(Form("HFold%d", Count), "", NReco, 0, NReco);
+   HResult->Sumw2();
+
+   for(int iG = 1; iG <= NGen; iG++)
+   {
+      double N = 0;
+      for(int iR = 1; iR <= NReco; iR++)
+         N = N + HResponse->GetBinContent(iR, iG);
+
+      if(N == 0)
+         continue;
+
+      double VG = HGen->GetBinContent(iG);
+      double EG = HGen->GetBinError(iG);
+
+      for(int iR = 1; iR <= NReco; iR++)
+      {
+         double VR = HResult->GetBinContent(iR);
+         double ER = HResult->GetBinError(iR);
+
+         double R = HResponse->GetBinContent(iR, iG);
+
+         VR = VR + VG * R / N;
+         ER = sqrt(ER * ER + (EG * R / N) * (EG * R / N));
+
+         HResult->SetBinContent(iR, VR);
+         HResult->SetBinError(iR, ER);
+
+         // HResult->Fill(iR - 1, HResponse->GetBinContent(iR, iG) * HGen->GetBinContent(iG) / N);
+      }
+   }
+
+   return HResult;
+}
+
+vector<int> ListIterations(string FileName)
+{
+   vector<int> Result;
+
+   TFile File(FileName.c_str());
+
+   TIter Iter(File.GetListOfKeys());
+   for(TKey *Key = (TKey *)Iter(); Key != nullptr; Key = (TKey *)Iter())
+   {
+      string Name = Key->ReadObj()->GetName();
+      if(Name.rfind("HUnfoldedBayes", 0) != 0)
+         continue;
+
+      Result.push_back(stoi(Name.substr(14)));
+   }
+
+   File.Close();
+
+   return Result;
+}
+
