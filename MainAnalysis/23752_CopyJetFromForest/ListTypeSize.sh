06a6b473fc7921408f5cd3c4a2ba34e7fe9b4700
FHead <chen.yi.first@gmail.com>
2021-12-29 11:22:17 +0100

    updated support script

--- MainAnalysis/23752_CopyJetFromForest/ListTypeSize.sh
+++ MainAnalysis/23752_CopyJetFromForest/ListTypeSize.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-for i in `ls Output/*root | cut -d '_' -f 1 | sort | uniq`
+for i in `ls Output/*root | cut -d '_' -f 1 | sort | uniq | grep -v 'Output/Run'`
+do
+   echo $i: `du -csh ${i}_*root | grep total`
+done
+
+for i in `ls Output/Run_*root | cut -d '_' -f 1,2 | sort | uniq`
 do
    echo $i: `du -csh ${i}_*root | grep total`
 done
--- MainAnalysis/23752_CopyJetFromForest/QueryProgress.sh
+++ MainAnalysis/23752_CopyJetFromForest/QueryProgress.sh
@@ -1,1 +1,4 @@
-condor_q -nobatch | cat | grep Run_PbPbMC[A-Za-z]*_Part -o | sort | uniq -c
+echo by type
+condor_q -nobatch | cat | grep Run_[A-Za-z]*_Part -o | cut -d '_' -f 2 | sort | uniq -c
+echo
+
