8ae9d979414d0b6123f540dedfed2a41370f6d05
FHead <chen.yi.first@gmail.com>
2022-01-02 16:03:29 +0100

    adding DH file option to script

--- MainAnalysis/24112_BrilCalc/CheckJSON.sh
+++ MainAnalysis/24112_BrilCalc/CheckJSON.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 Input=$1
+DH=$2
+
+if [[ "$DH" == "" ]]; then
+   DH=GlobalSetting.dh
+fi
 
 brilcalc lumi --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_BRIL.json -i ${Input} -o temp.csv
 BrilValue=`grep -v "^#" temp.csv | cut -d ',' -f 6 | Sum`
@@ -14,6 +19,6 @@ Tag=`basename $Input | sed "s/_Nominal//" | sed "s/_JSON.txt//"`
 
 echo $Tag $BrilValue $PhysicsValue
 
-DHSet GlobalSetting.dh Lumi ${Tag/Fine}_BRIL double $BrilValue
-DHSet GlobalSetting.dh Lumi ${Tag/Fine}_PHYSICS double $PhysicsValue
+DHSet $DH Lumi ${Tag/Fine}_BRIL double $BrilValue
+DHSet $DH Lumi ${Tag/Fine}_PHYSICS double $PhysicsValue
 
