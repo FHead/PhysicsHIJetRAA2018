d058c89e82f6580bc23458fb8c6b6eb4689f5529
FHead <chen.yi.first@gmail.com>
2022-01-03 11:15:38 +0100

    change lower bound to 80

--- MainAnalysis/23763_BeautifulMatrix/RunFile.sh
+++ MainAnalysis/23763_BeautifulMatrix/RunFile.sh
@@ -48,10 +48,10 @@ do
          --Input Input/${PRCN} \
          --Output Plots/QualityPlots_${PRC}${Suffix}.pdf --FinalOutput Plots/${PRC}${Suffix}.pdf \
          --UseActualZMax true \
-         --WorldXMin 40 --WorldXMax 1500 --LogX true \
-         --WorldYMin 40 --WorldYMax 1500 --LogY true \
+         --WorldXMin 80 --WorldXMax 1500 --LogX true \
+         --WorldYMin 80 --WorldYMax 1500 --LogY true \
          --WorldZMin 0.0001 --WorldZMax 0.1 --LogZ true \
-         --GenPrimaryMin 40 --GenPrimaryMax 1500 --RecoPrimaryMin 40 --RecoPrimaryMax 1500 \
+         --GenPrimaryMin 80 --GenPrimaryMax 1500 --RecoPrimaryMin 80 --RecoPrimaryMax 1500 \
          --Primary "Jet p_{T} (GeV)" --Binning "None" --RowNormalize true \
          --XAxis 303 --YAxis 303 \
          --GroupX 1 --GroupY 1 --Row 1 --Column 1
