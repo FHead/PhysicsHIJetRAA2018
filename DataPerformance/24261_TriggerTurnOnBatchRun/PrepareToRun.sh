a9eddc94070015ff3470022837672fc52ec8bc75
FHead <chen.yi.first@gmail.com>
2022-01-04 14:10:09 +0100

    trigger turn on batch

---
+++ DataPerformance/24261_TriggerTurnOnBatchRun/PrepareToRun.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+File=Submit.condor
+rm -f ${File}
+
+ToRun="PbPbMBData"
+# ToRunR="1 2 3 5 6 7 8 9"
+ToRunR="4"
+Group=40
+
+echo "Setting up submission to run on the following list"
+echo "$ToRun"
+echo "with the following jet radius tags"
+echo "$ToRunR"
+echo "and number of forest files per job"
+echo "$Group"
+
+echo "Universe              = vanilla" >> ${File}
+echo "Executable            = /afs/cern.ch/user/c/chenyi/PhysicsWorkspace/HIJetRAA2018/DataPerformance/24261_TriggerTurnOnBatchRun/Run.sh" >> ${File}
+echo "should_transfer_files = NO" >> ${File}
+echo "+JobFlavour           = \"longlunch\"" >> ${File}
+echo "" >> ${File}
+echo "" >> ${File}
+
+Count=0
+
+for Type in $ToRun
+do
+   Centrality=
+   if [[ "$Type" == "PbPbData" ]]; then
+      Centrality="0to10 10to30 30to50 50to70 70to90 50to90"
+   elif [[ "$Type" == "PbPbMBData" ]]; then
+      Centrality="Inclusive"
+   elif [[ "$Type" == "PPData" ]]; then
+      Centrality="Inclusive"
+   fi
+
+   for R in $ToRunR
+   do
+      LocationBase=`DHQuery GlobalSetting.dh Sample $Type$R`
+
+      for j in `find "${LocationBase}/" | grep root$ | Reformat $Group | tr ' ' ',' | sed "s/[ ,]*$//g"`
+      do
+         Count=`echo $Count | AddConst 1`
+
+         # Arguments to Run.sh:
+         #    FileList Tag IsPP IsMB TriggerBase Trigger JetR
+         # Everything after the file list is in this extra arguments list
+
+         ExtraArguments="Default 0 0 X X $R"
+         if [[ "$Type" == "PbPbData" ]]; then
+            ExtraArguments="PbPbData_Part$Count 0 0 HLT_HIPuAK4CaloJet80Eta5p1 HLT_HIPuAK4CaloJet100Eta5p1 $R"
+         elif [[ "$Type" == "PbPbMBData" ]]; then
+            ExtraArguments="PbPbMBData_Part$Count 0 0 HLT_HIPuAK4CaloJet40Eta5p1 HLT_HIPuAK4CaloJet100Eta5p1 $R"
+         elif [[ "$Type" == "PPData" ]]; then
+            ExtraArguments="PPData_Part$Count 1 0 HLT_HIAK4PFJet60 HLT_HIAK4PFJet80 $R"
+         fi
+
+         echo "Arguments = $j  $ExtraArguments" >> ${File}
+         echo "Output    = Log/${Type}${R}Part${Count}.out" >> ${File}
+         echo "Error     = Log/${Type}${R}Part${Count}.err" >> ${File}
+         echo "Log       = Log/${Type}${R}Part${Count}.log" >> ${File}
+         echo "Queue" >> ${File}
+         echo "" >> ${File}
+      done
+      
+      echo "" >> ${File}
+      echo "" >> ${File}
+   done
+done
+
---
+++ DataPerformance/24261_TriggerTurnOnBatchRun/Run.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+cd /afs/cern.ch/work/c/chenyi/CMSSW/CMSSW_11_2_1_patch2/src/
+eval `scramv1 runtime -sh`
+cd -
+CodeBase=/afs/cern.ch/user/c/chenyi/PhysicsWorkspace/HIJetRAA2018/DataPerformance/24261_TriggerTurnOnBatchRun/
+export ProjectBase=/afs/cern.ch/work/c/chenyi//PhysicsWorkspace/HIJetRAA2018/
+export PATH=$PATH:/afs/cern.ch/user/c/chenyi/RootMacros/
+
+FileList=$1    # list of files to run over
+Tag=$2         # Tag to use for outputs
+IsPP=$3        # 0 = PbPb, 1 = pp NonUL, 2 = pp UL
+FromMB=$4      # if 1 use minimum bias
+IsMC=0         # Only do data at the moment
+TriggerBase=$5 # Base trigger
+Trigger=$6     # Target trigger
+JetR=$7        # What Jet R to run over
+OutputBase=/eos/cms/store/group/phys_heavyions/chenyi/PhysicsWorkspace/HIJetRAA2018/DataPerformance/24261_TriggerTurnOnBatchRun/
+
+
+echo "Running with the following inputs"
+echo "   FileList = $FileList"
+echo "   Tag = $Tag"
+echo "   IsPP = $IsPP"
+echo "   FromMB = $FromMB"
+echo "   IsMC = $IsMC"
+echo "   TriggerBase = $TriggerBase"
+echo "   Trigger = $Trigger"
+echo "   JetR = $JetR"
+
+Centrality=
+if [[ "$Tag" == "PbPbData"* ]]; then
+   Centrality=`DHQuery $CodeBase/GlobalSetting.dh Global Centrality`
+elif [[ "$Tag" == "PbPbMBData"* ]]; then
+   Centrality=Inclusive
+elif [[ "$Tag" == "PbPbData"* ]]; then
+   Centrality=Inclusive
+fi
+
+if [[ "$IsPP" != "0" ]]; then
+   CutString="--DoBaselineCutAA false --DoBaselineCutPP true"
+else
+   CutString="--DoBaselineCutAA true --DoBaselineCutPP false"
+fi
+
+JetRValue=`DHQuery $CodeBase/GlobalSetting.dh JetR ${JetR}`
+
+JECBase=$ProjectBase/CommonCode/jec/
+
+JetTree=
+JECTag=
+Recluster=false
+ReEvaluate=false
+DoJetID=false
+JetIDKeyBase="none"
+if [[ "$IsMC" == "0" ]] && [[ "$IsPP" == "0" ]]; then
+   JECTag="Autumn18_HI_RAAV2_DATA"
+   JECList="${JECBase}/${JECTag}/${JECTag}_L2Relative_AK${JetR}PF.txt","${JECBase}/Phi_24252/PhiCorrectionGenLowNoThreshold_AK${JetR}PF.txt","${JECBase}/${JECTag}/${JECTag}_L2L3Residual_AK${JetR}PF.txt"
+   JetTree="akCs${JetR}PFJetAnalyzer/t"
+   Recluster=false
+   ReEvaluate=true
+   DoJetID=true
+   JetIDKeyBase=PbPbData_R${JetR}_Centrality0to10
+elif [[ "$IsMC" == "0" ]] && [[ "$IsPP" == "1" ]]; then
+   JECTag="Spring18_ppRef5TeV_RAAV2_DATA"
+   JECList="${JECBase}/${JECTag}/${JECTag}_L2Relative_AK${JetR}PF.txt","${JECBase}/${JECTag}/${JECTag}_L2L3Residual_AK${JetR}PF.txt"
+   JetTree="ak${JetR}PFJetAnalyzer/t"
+   Recluster=false
+   ReEvaluate=false
+   DoJetID=true
+   JetIDKeyBase=PPData_R${JetR}_CentralityInclusive
+elif [[ "$IsMC" == "0" ]] && [[ "$IsPP" == "2" ]]; then
+   JECTag="Summer20UL17_ppRef5TeV_RAAV1_DATA"
+   JECList="${JECBase}/${JECTag}/${JECTag}_L2Relative_AK${JetR}PF.txt","${JECBase}/${JECTag}/${JECTag}_L2L3Residual_AK${JetR}PF.txt"
+   JetTree="ak${JetR}PFJetAnalyzer/t"
+   Recluster=false
+   ReEvaluate=false
+   DoJetID=true
+   JetIDKeyBase=PPData_R${JetR}_CentralityInclusive
+fi
+
+for C in $Centrality
+do
+   CMin=`DHQuery $CodeBase/GlobalSetting.dh CentralityMin $C`
+   CMax=`DHQuery $CodeBase/GlobalSetting.dh CentralityMax $C`
+
+   time $CodeBase/Source/Execute --Input $FileList \
+      --OutputBase Result_${Tag}_R${JetR}_Centrality${C} \
+      --Base ${TriggerBase} \
+      --Trigger ${Trigger} \
+      --Jet "${JetTree}" --JetR ${JetRValue} \
+      --Recluster ${Recluster} \
+      --ReEvaluateCentrality $ReEvaluate --CentralityMin ${CMin} --CentralityMax ${CMax} \
+      --EtaMin -2.0 --EtaMax 2.0 \
+      ${CutString} \
+      --DoJetID $DoJetID --JetIDKeyBase $JetIDKeyBase \
+      --JEC $JECList --WriteTree true
+   mv Result_*root $OutputBase/
+done
+
+
