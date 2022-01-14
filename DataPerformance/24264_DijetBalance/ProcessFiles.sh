4a4cd6b9a530690b2127d7d18480158ea6a1cb37
FHead <chen.yi.first@gmail.com>
2022-01-14 14:32:43 +0100

    dijet balance

--- DataPerformance/24264_DijetBalance/ProcessFiles.sh
+++ DataPerformance/24264_DijetBalance/ProcessFiles.sh
@@ -8,17 +8,25 @@ do
    # ./Execute --Input /tmp/chenyi/Input.root --Output /tmp/chenyi/Output.root --IsMC 0
    # cp /tmp/chenyi/Output.root Output/PbPbDataR${i}.root
 
-   # cp Input/PbPbData_R${i}_Centrality50to90.root /tmp/chenyi/Input.root
+   # cp Input/PbPbMC_R${i}_Centrality50to90.root /tmp/chenyi/Input.root
    # ./Execute --Input /tmp/chenyi/Input.root --Output /tmp/chenyi/Output.root --IsMC 1
    # cp /tmp/chenyi/Output.root Output/PbPbMCR${i}.root
    
-   cp Input/PPDataEOY_R${i}_CentralityInclusive.root /tmp/chenyi/Input.root
+   cp Input/PbPbData_R${i}_Centrality70to90.root /tmp/chenyi/Input.root
    ./Execute --Input /tmp/chenyi/Input.root --Output /tmp/chenyi/Output.root --IsMC 0
-   cp /tmp/chenyi/Output.root Output/PPDataEOYR${i}.root
+   cp /tmp/chenyi/Output.root Output/PbPbData2R${i}.root
 
-   cp Input/PPMCEOY_R${i}_CentralityInclusive.root /tmp/chenyi/Input.root
+   cp Input/PbPbMC_R${i}_Centrality70to90.root /tmp/chenyi/Input.root
    ./Execute --Input /tmp/chenyi/Input.root --Output /tmp/chenyi/Output.root --IsMC 1
-   cp /tmp/chenyi/Output.root Output/PPMCEOYR${i}.root
+   cp /tmp/chenyi/Output.root Output/PbPbMC2R${i}.root
+   
+   # cp Input/PPDataEOY_R${i}_CentralityInclusive.root /tmp/chenyi/Input.root
+   # ./Execute --Input /tmp/chenyi/Input.root --Output /tmp/chenyi/Output.root --IsMC 0
+   # cp /tmp/chenyi/Output.root Output/PPDataEOYR${i}.root
+
+   # cp Input/PPMCEOY_R${i}_CentralityInclusive.root /tmp/chenyi/Input.root
+   # ./Execute --Input /tmp/chenyi/Input.root --Output /tmp/chenyi/Output.root --IsMC 1
+   # cp /tmp/chenyi/Output.root Output/PPMCEOYR${i}.root
    
    # cp Input/PPData_R${i}_CentralityInclusive.root /tmp/chenyi/Input.root
    # ./Execute --Input /tmp/chenyi/Input.root --Output /tmp/chenyi/Output.root --IsMC 0
